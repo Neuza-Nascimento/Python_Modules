4b8d3dba29c224722ad4b9ee9bf471df6c55e95e
Neuza Teresa Do Nascimento <nedo-nas@c1r12s2.42porto.com>
2026-06-26 10:55:12 +0100

    module 04 finish

--- module04/ex3/ft_vault_security.py
+++ module04/ex3/ft_vault_security.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
 
 
-def secure_archive(name_file: str, op: str, content: str | None) -> tuple:
+def secure_archive(name_file: str, op: str = "r", content: str = ""
+                   ) -> tuple[bool, str]:
     try:
-        info = [False, None]
         if (op == "r"):
-         with open(name_file, "r") as file:
-            info[1] = file.read()
-            info[0] = True
-        elif (op == "w"):
+            with open(name_file, "r") as file:
+                return (True, file.read())
+        else:
             with open(name_file, "w") as file:
                 file.write(content)
-                info[0] = True
-                info[1] = content     
-    except Exception as e:
-        info[0] = False
-        info[1] = e
-    return info
-  
+                return (True, "Content successfully written to file")
+    except (FileNotFoundError, PermissionError) as e:
+        return (False, str(e))
+
 
 def main() -> None:
     print("=== Cyber Archives Security ===")
 
-    print("Using 'secure_archive' to read from a nonexistent file:")
-    print(secure_archive("/not/existing/file", "r"))
+    print("\nUsing 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive("/not/existing/file"))
 
-    print("Using 'secure_archive' to read from an inaccessible file:")
-    print(secure_archive("/etc/master.passwd", "r"))
+    print("\nUsing 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("/etc/master.passwd"))
 
-    print("Using 'secure_archive' to read from a regular file:")
-    print(secure_archive("teste", "r"))
+    print("\nUsing 'secure_archive' to read from a regular file:")
+    result = (secure_archive("teste"))
+    print(result)
 
-    print("Using 'secure_archive' to write previous content to a new file:")
-    print(secure_archive("text", "w","Content successfully written to file"))
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
+    print(secure_archive("text", "w", result[1]))
 
 
 if __name__ == "__main__":
