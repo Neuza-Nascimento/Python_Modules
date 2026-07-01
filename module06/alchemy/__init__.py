0902473e5d4cbd2cda6af942c997fabd2ea864cd
Neuza Teresa Do Nascimento <nedo-nas@c2r6s9.42porto.com>
2026-07-01 16:59:43 +0100

    start module06

---
+++ module06/alchemy/__init__.py
@@ -0,0 +1,4 @@
+from .elements import create_air
+from .potions import healing_potion as heal, strength_potion
+
+__all__: list[str] = ["create_air", "heal", "strength_potion"]
---
+++ module06/alchemy/elements.py
@@ -0,0 +1,6 @@
+def create_earth() -> str:
+    return ("Air element created")
+
+
+def create_air() -> str:
+    return ("Air element created")
