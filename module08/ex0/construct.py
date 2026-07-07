606a3ee798592b69378d838a405fe337d82a4a7e
Neuza Teresa Do Nascimento <nedo-nas@c2r6s9.42porto.com>
2026-07-07 19:36:54 +0100

    finish module08

--- module08/ex0/construct.py
+++ module08/ex0/construct.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python3
-
 import sys
 import os
 import site
 
 
-#procura saber se estamos diante do ambiente virtual ou nao
-#.prefix: Contém o caminho da pasta raiz do ambiente Python +
-# que está a correr o script neste momento.
-#.base_prefix: Contém o caminho da instalação original e global do Python na tua máquina
 def is_virtual_ambient() -> bool:
     return sys.prefix != sys.base_prefix or 'VIRTUAL_ENV' in os.environ
 
@@ -16,29 +10,24 @@ def is_virtual_ambient() -> bool:
 def main() -> None:
     if not is_virtual_ambient():
         print("MATRIX STATUS: You're still plugged in\n")
-        #Devolve o caminho absoluto do binário (o executável) do Python que iniciou o programa.
-        #/usr/bin/python3
-        print(f"Current Python: {sys.executable}") 
-        print(f"Virtual Environment: None detected\n")
+        print(f"Current Python: {sys.executable}")
+        print("Virtual Environment: None detected\n")
         print("WARNING: You're in the global environment!")
         print("The machines can see everything you install\n")
         print("To enter the construct, run:")
         print("python -m venv matrix_env")
         print("source matrix_env/bin/activate # On Unix")
-        print("matrix_env\Scripts\\activate  # On Windows\n")
+        print("matrix_env\\Scripts\\activate  # On Windows\n")
         print("Then run this program again.")
     else:
         print("MATRIX STATUS: Welcome to the construct\n")
-        #/path/to/matrix_env/bin/python
         print(f"Current Python: {sys.executable}")
-        # Pega num caminho completo e extrai apenas o nome do último elemento
         print(f"Virtual Environment: {os.path.basename(sys.prefix)}")
         print(f"Environment Path: {sys.prefix}\n")
-        print(f"SUCCESS: You're in an isolated environment!")
-        print(f"Safe to install packages without affecting the global system.\n")
+        print("SUCCESS: You're in an isolated environment!")
+        print("Safe to install packages without affecting"
+              "the global system.\n")
         print("Package installation path:")
-        #Retorna uma lista de caminhos onde o Python vai procurar e instalar bibliotecas.
-        #permite mostrar ao utilizador a pasta site-packages
         print(site.getsitepackages()[0])
 
 
---
+++ module08/ex1/loading.py
@@ -0,0 +1,53 @@
+from importlib.metadata import version, PackageNotFoundError
+
+
+packages: list[tuple[str, str]] = [("pandas", "Data manipulation ready"),
+                                   ("numpy", "Numerical computation ready"),
+                                   ("matplotlib", "Visualization ready")]
+
+
+def check_dependencies() -> bool:
+    isPackage = True
+    for package, description in packages:
+        try:
+            print(f"[OK] {package} ({version(package)}) - {description}")
+        except (PackageNotFoundError):
+            print(f"{package} is missing")
+            isPackage = False
+    return isPackage
+
+
+def main() -> None:
+    print("\nLOADING STATUS: Loading programs...\n")
+    if check_dependencies():
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import pandas as pd
+        print("Analyzing Matrix data...")
+        data = np.random.randn(250, 4)
+        colunas: list[str] = ["Veronique", "Gabriel", "Felyppe", "Neuza"]
+        print("Processing 1000 data points...")
+        df = pd.DataFrame(data=data, columns=colunas)
+        de_sum = df.cumsum()
+        print("Generating visualization..")
+        plt.figure(figsize=(10, 6))
+        plt.plot(de_sum["Neuza"], label="Neuza", color="blue")
+        plt.plot(de_sum["Felyppe"], label="Felyppe", color="green")
+        plt.plot(de_sum["Gabriel"], label="Gabriel", color="orange")
+        plt.plot(de_sum["Veronique"], label="Veronique", color="pink")
+        plt.title("Matrix data", fontsize=14)
+        plt.grid(True, linestyle="--", alpha=0.6)
+        plt.legend()
+        plt.savefig(" matrix_analysis.png", dpi=100)
+        print("Analysis complete!")
+        print("Results saved to: matrix_analysis.png")
+    else:
+        print("\nTo install missing dependencies use:")
+        print("For pip:")
+        print(" 'pip install -r requirements.txt'")
+        print("For Proety:")
+        print(" 'for poetry run python loading.py'")
+
+
+if __name__ == "__main__":
+    main()
